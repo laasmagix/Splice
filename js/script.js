8496a75aba984775842d946463c7d56878522333
Nazar Dzuba <nazardzuba59@gmail.com>
2024-02-16 13:43:10 +0200

    created and adapted main-block

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 ("use strict");
+document.addEventListener("click", documentClick);
+function documentClick(e) {
+  const targetItem = e.target;
+  if (targetItem.closest(".icon-menu")) {
+    document.documentElement.classList.toggle("menu-open");
+  }
+}
+
 function ibg() {
   let ibg = document.querySelectorAll(".ibg");
   for (var i = 0; i < ibg.length; i++) {
@@ -9,11 +17,3 @@ function ibg() {
   }
 }
 ibg();
-
-document.addEventListener("click", documentClick);
-function documentClick(e) {
-  const targetItem = e.target;
-  if (targetItem.closest(".icon-menu")) {
-    document.documentElement.classList.toggle("menu-open");
-  }
-}
